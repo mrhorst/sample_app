d917701abd99eab0798f9a29919185f21ccea077
Matheus Horst <mhorst31@gmail.com>
2016-07-25 14:32:08 -0400

    change home and help pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get home" do
     get :home
     assert_response :success
@@ -10,5 +11,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :help
     assert_response :success
   end
-
 end
